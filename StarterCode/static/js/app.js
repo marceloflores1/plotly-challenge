d886a4960667d7a963d44260588491302be02f81
Marcelo Flores <79871752+marceloflores1@users.noreply.github.com>
2021-07-03 16:08:57 -0500

    Modifying app.js and html

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,71 +1,68 @@
 var dropdownSel = d3.select("#selDataset");
 var demographicSel = d3.select("#sample-metadata")
 
-d3.json("samples.json").then((importedData) => {
-    // console.log(importedData);
-    var data = importedData;
-  
-    // Sort the data array using the greekSearchResults value
-    var sampleData = data.samples[0];
-    
-    // data.sort(function(a, b) {
-    //   return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-    // });
+d3.json("samples.json").then((importedData) => {   
     
     var dropItems = importedData.names.map((row) => parseInt(row));
-    dropdownNames(dropItems);
+    dropdownNames(dropItems); 
+    demographicSel.html("Please select a Test Subject ID");
+});
 
-    var metadataSel = dropdownSel.property("value");
-    var allMetadata = importedData.metadata
-    console.log(metadataSel);
-    console.log(allMetadata);
-    demographicInfo(allMetadata, metadataSel);
+function optionChanged(inputID) {
 
-    // Slice the first 10 objects for plotting
-    var slicedSampleValues = sampleData.sample_values.slice(0, 10);
-    var slicedOtuIds = sampleData.otu_ids.slice(0,10);
-    var slicedOutLabels = sampleData.otu_labels.slice(0,10);
-    
-    // Reverse the array due to Plotly's defaults
-  
-    // Trace1 for the Greek Data
-    var trace1 = {
-      x: slicedSampleValues,
-      y: slicedOutLabels,
-      text: slicedOtuIds,
-      name: "Sample ID",
-      type: "bar",
-      orientation: "h"
-    };
-  
-    // data
-    var chartData = [trace1];
-  
-    // Apply the group bar mode to the layout
-    var layout = {
-      title: "Samples",
-      
-    };
-  
-    // Render the plot to the div tag with id "plot"
-    Plotly.newPlot("bar", chartData, layout);
-  });
+    d3.json("samples.json").then((importedData) => {
+        var allMetadata = importedData.metadata
+        console.log(inputID);
+        console.log(allMetadata);
+        demographicInfo(allMetadata, inputID);
+
+        var sampleData = importedData.samples.find((sample) => sample.id === inputID);
+        var slicedSampleValues = sampleData.sample_values.slice(0, 10).reverse();
+        var slicedOtuIds = sampleData.otu_ids.slice(0,10).map((row) => `OTU ${String(row)}`).reverse();
+        var slicedOutLabels = sampleData.otu_labels.slice(0,10);
+
+        // Trace1 
+        var trace1 = {
+        x: slicedSampleValues,
+        y: slicedOtuIds,
+        text: slicedOutLabels,
+        name: "Sample ID",
+        type: "bar",
+        orientation: "h"
+        };
+        var chartData = [trace1];
+
+        var layout = {
+        title: "Top 10 Bacteria Cultures Found",
+        };
+
+        Plotly.newPlot("bar", chartData, layout);
+    })
+};
 
   // Function to add dropdown items
   function dropdownNames (dropItems) {
     dropItems.forEach((name) => {
-        dropdownSel.append('option').text(name);
+        dropdownSel.append('option').text(name).property("value", name);
     })
   };
 
-  function demographicInfo (allMetadata, metadataSel) {
+  function demographicInfo (allMetadata, inputID) {
     demographicSel.html("");
     allMetadata.forEach(row => {
-        if(row.id === parseInt(metadataSel)) {
+        if(row.id === parseInt(inputID)) {
             for(var [key,value] of Object.entries(row)) {
                 var upperKey = key.toUpperCase();
                 demographicSel.append('p').text(`${upperKey}: ${value}`);
             }
-        }
+        } 
     })
   };
+
+// THIS WILL BE MY FUNCTION TO CHANGE THE NAME OF THE WEIRD ; LIST OF VALUES
+//   function renameSplit(list) {
+//     newlist []  
+//     list.forEach(item => {
+//         newlist.append()
+//     })
+//   };
