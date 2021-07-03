205b4938764f9788d1fa9aaf129a56ff9045eb45
Marcelo Flores <79871752+marceloflores1@users.noreply.github.com>
2021-07-03 12:26:40 -0500

    Modifying app.js and html

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,18 +1,23 @@
+var dropdownSel = d3.select("#selDataset");
+
 d3.json("samples.json").then((importedData) => {
     // console.log(importedData);
     var data = importedData;
   
     // Sort the data array using the greekSearchResults value
-    sampleData = data.samples[0];
+    var sampleData = data.samples[0];
     
     // data.sort(function(a, b) {
     //   return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
     // });
-  
+    
+    var names = data.names.map((row) => parseInt(row));
+    dropdownNames(names);
+
     // Slice the first 10 objects for plotting
-    slicedSampleValues = sampleData.sample_values.slice(0, 10);
-    slicedOtuIds = sampleData.otu_ids.slice(0,10);
-    slicedOutLabels = sampleData.otu_labels.slice(0,10);
+    var slicedSampleValues = sampleData.sample_values.slice(0, 10);
+    var slicedOtuIds = sampleData.otu_ids.slice(0,10);
+    var slicedOutLabels = sampleData.otu_labels.slice(0,10);
   
     // Reverse the array due to Plotly's defaults
   
@@ -38,4 +43,10 @@ d3.json("samples.json").then((importedData) => {
     // Render the plot to the div tag with id "plot"
     Plotly.newPlot("bar", chartData, layout);
   });
-  
+
+  // Function to add dropdown items
+  function dropdownNames (data) {
+    data.forEach((name) => {
+        d3.dropdownSel.append('option').text(name);
+    })
+  };
