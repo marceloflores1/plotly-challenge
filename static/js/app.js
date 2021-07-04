51efee68287af54ce5dc04f7ad7af83edbe4aa2b
Marcelo Flores <79871752+marceloflores1@users.noreply.github.com>
2021-07-03 23:11:41 -0500

    Cleaning and code formatting in app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,31 +1,47 @@
 var dropdownSel = d3.select("#selDataset");
 var demographicSel = d3.select("#sample-metadata")
 
+// Loading dropdown items
 d3.json("samples.json").then((importedData) => {   
     
     var dropItems = importedData.names.map((row) => parseInt(row));
-    dropdownNames(dropItems); 
+    dropItems.forEach((name) => {
+        dropdownSel.append('option').text(name).property("value", name);
+    });
     demographicSel.html("Please select a Test Subject ID");
 });
 
+// Function executed when changing dropdown item
 function optionChanged(inputID) {
 
     d3.json("samples.json").then((importedData) => {
+                
+        // Demographic Info
         var allMetadata = importedData.metadata
-        console.log(inputID);
-        console.log(allMetadata);
-        demographicInfo(allMetadata, inputID);
+        demographicSel.html("");
+        allMetadata.forEach(row => {
+            if(row.id === parseInt(inputID)) {
+                for(var [key,value] of Object.entries(row)) {
+                    var upperKey = key.toUpperCase();
+                    demographicSel.append('p').text(`${upperKey}: ${value}`);
+                }
+            }
+        });
 
+        // Variables used to plot
         var sampleData = importedData.samples.find((sample) => sample.id === inputID);
-        var slicedSampleValues = sampleData.sample_values.slice(0, 10).reverse();
-        var slicedOtuIds = sampleData.otu_ids.slice(0,10).map((row) => `OTU ${String(row)}`).reverse();
-        var slicedOutLabels = renameSplit(sampleData.otu_labels.slice(0,10));
+        var sampleValues = sampleData.sample_values
+        var otuIds = sampleData.otu_ids
+        var otuLabels = renameSplit(sampleData.otu_labels) 
+        var slicedSampleValues = sampleValues.slice(0, 10).reverse();
+        var slicedOtuIds = otuIds.slice(0,10).map((row) => `OTU ${String(row)}`).reverse();
+        var slicedOtuLabels = otuLabels.slice(0,10);
 
         // Bar Chart 
         var trace1 = {
         x: slicedSampleValues,
         y: slicedOtuIds,
-        text: slicedOutLabels,
+        text: slicedOtuLabels,
         name: "Sample ID",
         type: "bar",
         orientation: "h"
@@ -40,13 +56,13 @@ function optionChanged(inputID) {
 
         // Bubble Chart
         var trace2 = {
-            x: sampleData.otu_ids,
-            y: sampleData.sample_values,
-            text: renameSplit(sampleData.otu_labels),
+            x: otuIds,
+            y: sampleValues,
+            text: otuLabels,
             mode: 'markers',
             marker: {
-                size: sampleData.sample_values,
-                color: sampleData.otu_ids,
+                size: sampleValues,
+                color: otuIds,
                 colorscale: [
                     [0, 'rgb(0,180,255)'],
                     [0.5, 'rgb(0,255,50)'],
@@ -59,40 +75,20 @@ function optionChanged(inputID) {
 
         var bubbleLayout = {
             title: "Bacteria Cultures Per Sample",
-            xaxis: {title: "Operational Taxonomic Unit (OTU) ID"},
-            yaxis: {title: "Sample Values"},
+            xaxis: {title: "OTU ID"}
         };
 
         Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
-        // // Gauge Chart
+        // Gauge Chart
         // var trace3 = {
         //    
         // }
     })
 };
 
-  // Function to add dropdown items
-  function dropdownNames (dropItems) {
-    dropItems.forEach((name) => {
-        dropdownSel.append('option').text(name).property("value", name);
-    })
-  };
-
-  function demographicInfo (allMetadata, inputID) {
-    demographicSel.html("");
-    allMetadata.forEach(row => {
-        if(row.id === parseInt(inputID)) {
-            for(var [key,value] of Object.entries(row)) {
-                var upperKey = key.toUpperCase();
-                demographicSel.append('p').text(`${upperKey}: ${value}`);
-            }
-        } 
-    })
-  };
-
-  function renameSplit(list) {
-    var newlist = [];
-    list.forEach(item => newlist.push(item.replaceAll(";", ", ")))
-    return newlist;
-  };
+function renameSplit(list) {
+var newlist = [];
+list.forEach(item => newlist.push(item.replaceAll(";", ", ")))
+return newlist;
+};
